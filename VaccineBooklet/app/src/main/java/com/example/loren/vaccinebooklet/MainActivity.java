370ab9b9136b7269a93b1a8a937549836fc1ac13
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2017-06-23 23:50:40 +0200

    Added allert dialog for the user choice

--- VaccineBooklet/app/src/main/java/com/example/loren/vaccinebooklet/MainActivity.java
+++ VaccineBooklet/app/src/main/java/com/example/loren/vaccinebooklet/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.loren.vaccinebooklet;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
@@ -11,6 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.loren.vaccinebooklet.utils.Utils;
@@ -76,18 +86,63 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.add_user) {
-            // Handle the camera action
+
+            final Item[] items;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                items = new Item[]{
+                        new Item(getString(R.string.type_user1),
+                                getResources().getDrawable(R.drawable.ic_person, null)),
+                        new Item(getString(R.string.type_user2),
+                                getResources().getDrawable(R.drawable.ic_pet_print, null))
+                };
+            } else {
+                items = new Item[]{
+                        new Item(getString(R.string.type_user1),
+                                getResources().getDrawable(R.drawable.ic_person)),
+                        new Item(getString(R.string.type_user2),
+                                getResources().getDrawable(R.drawable.ic_pet_print))
+                };
+            }
+            ListAdapter adapter = new ArrayAdapter<Item>(
+                    this,
+                    android.R.layout.select_dialog_item,
+                    android.R.id.text1,
+                    items){
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    //Use super class to create the View
+                    View myView = super.getView(position, convertView, parent);
+                    TextView myTextView = (TextView)myView.findViewById(android.R.id.text1);
+
+                    myTextView.setCompoundDrawablesWithIntrinsicBounds(items[position].icon, null, null, null);
+
+                    //Add margin between image and text (support various screen densities)
+                    int dp5 = (int) (10 * getResources().getDisplayMetrics().density + 0.5f);
+                    myTextView.setCompoundDrawablePadding(dp5);
+
+                    return myView;
+                }
+            };
+
+
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.choice_person_pet))
+                    .setAdapter(adapter, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            //...
+                        }
+                    }).show();
         } else if (id == R.id.log_out) {
+            // logout
             Utils.setLogged(MainActivity.this, false);
             Intent returnToLogin = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(returnToLogin);
+            Toast.makeText(MainActivity.this, message != null ? message : getString(R.string.logout_successfully), Toast.LENGTH_SHORT).show();
         }
 
        /* } else if (id == R.id.nav_slideshow) {
@@ -100,7 +155,7 @@ public class MainActivity extends AppCompatActivity
 
         }*/
 
-        Toast.makeText(MainActivity.this, message != null ? message : getString(R.string.logout_successfully), Toast.LENGTH_SHORT).show();
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
